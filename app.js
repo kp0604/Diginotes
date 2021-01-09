7dbf20b4a4355ab2b94a365500818a7d62511375
kp0604 <pandeykartikeya0604@gmail.com>
2021-01-10 00:05:28 +0530

    features-added features

--- app.js
+++ app.js
@@ -1,83 +1,93 @@
-console.log("hey")
+console.log("hey");
 
-const addTxt = document.getElementById("addTxt")
+const addTxt = document.getElementById("addTxt");
 
-const addBtn = document.getElementById("addBtn")
+const addBtn = document.getElementById("addBtn");
 
-const notes = document.getElementById("notes")
+const notes = document.getElementById("notes");
 
-const searchBtn = document.getElementById("searchBtn")
+const searchBtn = document.getElementById("searchBtn");
 
-const searchTxt = document.getElementById("searchTxt")
+const searchTxt = document.getElementById("searchTxt");
 
-showNotes()
-
-
+const titleTxt = document.getElementById("titleTxt");
 
 
-
+showNotes();
 
 function addNote() {
-    let notesFromLocal = JSON.parse(localStorage.getItem("localNotes"))
-
-    if (notesFromLocal == null) {
-        notesFromLocal = []
-        notesFromLocal.push(addTxt.value)
-        localStorage.setItem("localNotes", JSON.stringify(notesFromLocal))
+    let notesFromLocal = JSON.parse(localStorage.getItem("localNotes"));
+    let myobj={
+        title: titleTxt.value,
+         txt: addTxt.value,
 
     }
-
-    else {
-
-        notesFromLocal.push(addTxt.value)
-        localStorage.setItem("localNotes", JSON.stringify(notesFromLocal))
-
+    if (addTxt.value) {
+        if (notesFromLocal == null) {
+            notesFromLocal = [];
+            notesFromLocal.push(myobj);
+            localStorage.setItem("localNotes", JSON.stringify(notesFromLocal));
+        } else {
+            notesFromLocal.push(myobj);
+            localStorage.setItem("localNotes", JSON.stringify(notesFromLocal));
+        }
+    } else {
+        alert("Please add a note... ");
     }
 
-    showNotes()
-    addTxt.value = ""
+    showNotes();
+    addTxt.value = "";
+    titleTxt.value = "";
 }
-
 
 function showNotes() {
-
-    notesFromLocal = JSON.parse(localStorage.getItem("localNotes"))
-
-    notes.innerHTML=""
-    notesFromLocal.map((note, index) => {
-
-        notes.innerHTML += ` <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+    if ((notesFromLocal = JSON.parse(localStorage.getItem("localNotes")))) {
+        notes.innerHTML = "";
+        notesFromLocal.map((note, index) => {
+            notes.innerHTML += ` <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
     <div class="card-body">
-        <h5 class="card-title">Note-${index}</h5>
-        <p class="card-text"> ${note}</p>
+        <h5 class="card-title">${note.title}</h5>
+        <p class="card-text"> ${note.txt}</p>
         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete</button>
     </div>
-    </div>`
+    </div>`;
+        });
     }
-    )
 }
-
 
 function deleteNote(index) {
-
-
-    notesFromLocal.splice(index, 1)
-    localStorage.setItem("localNotes", JSON.stringify(notesFromLocal))
-    notes.innerHTML = ""
-    showNotes()
-
+    notesFromLocal.splice(index, 1);
+    localStorage.setItem("localNotes", JSON.stringify(notesFromLocal));
+    notes.innerHTML = "";
+    showNotes();
 }
 
+function search(e) {
+    
 
+    let noteCard = document.getElementsByClassName("noteCard");
 
-function searchoo() {
+    
+    Array.from(noteCard).map(function (element) {
+        let titleTag = element.getElementsByTagName("h5")[0].innerText
+        let paraTag = element.getElementsByTagName("p")[0].innerText
+       
+        if (titleTag.includes(searchTxt.value)||paraTag.includes(searchTxt.value)) {
+            element.style.display = "block"
 
-    console.log("searching...")
+            
+        }
+        else {
+            element.style.display = "none"
+            
+        }
+    e.preventDefault()    
+    })
 
-
-
+    
 }
 
-addBtn.addEventListener("click", addNote)
+addBtn.addEventListener("click", addNote);
 
-searchBtn.addEventListener("click", searchoo)
+// searchTxt.addEventListener("input", search);
+searchBtn.addEventListener("click", search);
