21cdc29889d1c62fc13fd065c282052946c222dc
kartikeya pandey <pandeykartikeya0604@gmail.com>
2021-04-06 13:15:36 +0530

    -bug fixes

--- app.js
+++ app.js
@@ -12,11 +12,10 @@ const searchTxt = document.getElementById("searchTxt");
 
 const titleTxt = document.getElementById("titleTxt");
 
-showNotes();
 
-function addNote() {
+function addNote(e) {
     let notesFromLocal = JSON.parse(localStorage.getItem("localNotes"));
-
+    
     if (addTxt.value) {
         let myobj = {
             title: titleTxt.value,
@@ -33,8 +32,9 @@ function addNote() {
     } else {
         alert("Enter all fields....");
     }
-
-    showNotes();
+    
+    e.preventDefault();
+    showNotes()
     addTxt.value = "";
     titleTxt.value = "";
 }
@@ -42,38 +42,38 @@ function addNote() {
 function showNotes() {
     let date = new Date()
     if ((notesFromLocal = JSON.parse(localStorage.getItem("localNotes")))) {
-        // notes.innerHTML = "";
+        notes.innerHTML = "";
         notesFromLocal.map((note, index) => {
 
             notes.innerHTML +=
 
-               ` <div class="col-lg-3 col-md-3 col-sm-12 m-0 my-2 " >
-
-                    <div class="d-flex card noteCard  bg-light justify-content-between " >
-
-                        <a class="d-flex card   card-header btn text-dark fs-5 fst-italic " data-bs-toggle="collapse" href="#collapseExample${index}" role="button" aria-expanded="false"
+            ` <div class="col-lg-3 col-md-3 col-sm-12 m-1 my-2 " >
+            
+            <div class="d-flex card noteCard  bg-light justify-content-between " >
+            
+            <a class="d-flex card   card-header btn text-dark fs-5 fst-italic " data-bs-toggle="collapse" href="#collapseExample${index}" role="button" aria-expanded="false"
                             aria-controls="collapseExample${index}" >
                             <p>${note.title} </p>
-
-                        </a>
+                            
+                            </a>
                         <div class="collapse card" id="collapseExample${index}">
-                            <div class="card card-body">
-                                <p class="card-text"> ${note.txt}</p>
+                        <div class="card card-body">
+                        <p class="card-text"> ${note.txt}</p>
                             </div>
-                        </div>
-
-                        <div class="d-flex justify-content-between  align-items-center align-contents-end mt-2">
-
+                            </div>
+                            
+                            <div class="d-flex justify-content-between mt-2">
+                            
                             <div class="mx-3" >Created : ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}</div>
                             <div>
-                                <button id="" onclick="" class="btn-sm btn-primary mx-1 "><i class="fa fa-pen"></i></button>
-                                <button id="${index}" onclick="deleteNote(this.id)" class="btn-sm btn-danger me-3 ">
+                            <button id="" onclick="" class="btn-sm btn-primary me-1 "><i class="fa fa-pen"></i></button>
+                            <button id="${index}" onclick="deleteNote(this.id)" class="btn-sm btn-danger me-3 ">
                                     <i class="fa fa-trash"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>`
+                                    </button>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>`
         }
         );
     }
@@ -91,15 +91,15 @@ function deleteNote(index) {
 }
 
 function search(e) {
-
+    
     let noteCard = document.getElementsByClassName("noteCard");
-
+    
     Array.from(noteCard).map(function (element) {
         let titleTag = element.getElementsByTagName("h5")[0].innerText;
-
+        
         if (
             titleTag.includes(searchTxt.value)
-
+            
         ) {
             element.setAttribute('style', 'display: block');
         } else {
@@ -108,6 +108,7 @@ function search(e) {
         e.preventDefault();
     });
 }
+showNotes();
 
-addBtn.addEventListener("click", addNote);
+addBtn.addEventListener("click", (e) => addNote(e));
 searchBtn.addEventListener("click", search);
